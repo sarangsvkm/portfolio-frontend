03f48245e6e99a0b215e6d0b41fe6207708cc8c6
SARANG S <sarangsvkm@gmail.com>
2026-04-26 18:24:01 +0530

    chore: remove AIFooter component as requested

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -23,7 +23,6 @@ import { resolveAssetUrl } from '../../utils/assetUrl';
 import VerificationGate from '../../components/public/VerificationGate';
 import { getVerifiedContact, type VerifiedContact } from '../../components/public/verificationStorage';
 import { getSocialIcon } from '../../utils/socialIcons';
-import AIFooter from '../../components/public/AIFooter';
 
 const PUBLIC_RESUME_CACHE_KEY = 'public_resume_cache';
 
@@ -509,7 +508,6 @@ export default function Home() {
            </motion.div>
         </div>
       </section>
-      <AIFooter />
     </div>
   );
 }
